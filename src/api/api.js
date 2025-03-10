3c79e91b7de51f6aab5fa6a39e5060d009f1244e
hamidreza <49334439+hamidrezasun@users.noreply.github.com>
2025-03-10 14:00:10 +0330

    bugfix

--- src/api/api.js
+++ src/api/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import { getToken } from './auth';
 
-const API_URL = 'http://192.168.1.37:8000';
+const API_URL = 'https://api.tooswasher.com';
 
 const api = axios.create({
   baseURL: API_URL,
